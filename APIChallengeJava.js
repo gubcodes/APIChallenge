c24078ecf933f254077fb560ef72335e1b91237f
gubcodes <sdlkfjsld@sdlkfjsldk.com>
2020-09-29 09:42:45 -0400

    9:30 9/29 (just cleaning up unused code/notes)

--- APIChallengeJava.js
+++ APIChallengeJava.js
@@ -1,10 +1,11 @@
-//assets for later
+//assets
 const url = 'https://cat-fact.herokuapp.com/facts';
 const imgUrl = 'https://api.thecatapi.com/v1/images/search';
 let divText = document.querySelector('.divText');
 let pawsButton = document.querySelector('.btn btn-outline-info');
 let catPic = document.querySelector('.catPic');
 let container = document.querySelector('.container');
+
 //random number generation
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -12,6 +13,7 @@ function getRandomInt(max) {
 let randomNumber = getRandomInt(199);
 console.log(randomNumber);
 
+//facts
 fetch(url)
     .then(function(result) {
         return result.json();
@@ -20,13 +22,12 @@ fetch(url)
         displayResults(json);
     });
 
-//WORKS:
-
 function displayResults(json) {
     console.log(json.all[`${randomNumber}`].text);
     divText.innerHTML='<p>' + json.all[`${randomNumber}`].text + '</p>';
 }
 
+//images
 fetch(imgUrl)
     .then(function(result) {
         return result.json();
@@ -40,28 +41,9 @@ function imageResults(data){
     catPic.setAttribute('src', data[0].url);
 }
 
+//button
 function refreshPage() { 
     location.reload();
 }
 
 pawsButton.addEventListener('click', refreshPage);
-
-
-// function tryThis(displayResults)
-
-// pawsButton.addEventListener('click', randomNumber);
-
-
-//FOR TESTING!!
-
-// function random(199) {
-//     const result = Math.floor(Math.random() * number);
-//     return result;
-//   }
-
-// function displayResults(json) {
-//     console.log(json.all[`${randomNumber}`].text);
-//     divText.innerHTML='<p>' + json.all[`${randomNumber}`].text + '</p>';
-// }
-
-// pawsButton.addEventListener('click', displayResults);
